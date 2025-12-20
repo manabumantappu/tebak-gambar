2258aef6138b0c9fbf4e92624ceb3ca145e9d097
manabumantappu <azishachigo@gmail.com>
2025-12-21 05:19:50 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,26 @@
 function jawab(benar) {
+  const popup = document.getElementById("popup");
+  const title = document.getElementById("popupTitle");
+  const text = document.getElementById("popupText");
+
   const soundBenar = document.getElementById("soundBenar");
   const soundSalah = document.getElementById("soundSalah");
 
   if (benar) {
+    title.innerHTML = "🎉 BENAR!";
+    text.innerHTML = "Hebat sekali! Kamu pintar 🌟";
     soundBenar.currentTime = 0;
     soundBenar.play();
-    alert("🎉 BENAR! Pintar sekali!");
   } else {
+    title.innerHTML = "😅 SALAH";
+    text.innerHTML = "Coba lagi ya, kamu pasti bisa!";
     soundSalah.currentTime = 0;
     soundSalah.play();
-    alert("😅 Salah, coba lagi ya!");
   }
+
+  popup.classList.remove("hidden");
+}
+
+function tutupPopup() {
+  document.getElementById("popup").classList.add("hidden");
 }
