0ddfd54598013ddf47e352fef0086c2e354ae0e2
manabumantappu <azishachigo@gmail.com>
2025-12-21 21:24:54 +0700

    Create script-buah.js

---
+++ script-buah.js
@@ -0,0 +1,107 @@
+const soal = [
+  {
+    gambar: "images/buah/apel.jpg",
+    tanya: "🍎 Buah apakah ini?",
+    opsi: ["Apel", "Jeruk", "Tomat"],
+    benar: 0,
+    info: "🍎 Apel adalah buah yang manis dan sehat."
+  },
+  {
+    gambar: "images/buah/pisang.jpg",
+    tanya: "🍌 Buah apakah ini?",
+    opsi: ["Mangga", "Pisang", "Pepaya"],
+    benar: 1,
+    info: "🍌 Pisang kaya energi dan mudah dicerna."
+  },
+  {
+    gambar: "images/buah/jeruk.jpg",
+    tanya: "🍊 Buah apakah ini?",
+    opsi: ["Jeruk", "Lemon", "Apel"],
+    benar: 0,
+    info: "🍊 Jeruk kaya vitamin C."
+  },
+  {
+    gambar: "images/buah/mangga.jpg",
+    tanya: "🥭 Buah apakah ini?",
+    opsi: ["Pepaya", "Mangga", "Nanas"],
+    benar: 1,
+    info: "🥭 Mangga rasanya manis dan segar."
+  },
+  {
+    gambar: "images/buah/anggur.jpg",
+    tanya: "🍇 Buah apakah ini?",
+    opsi: ["Anggur", "Blueberry", "Ceri"],
+    benar: 0,
+    info: "🍇 Anggur tumbuh bergerombol."
+  },
+  {
+    gambar: "images/buah/stroberi.jpg",
+    tanya: "🍓 Buah apakah ini?",
+    opsi: ["Raspberry", "Ceri", "Stroberi"],
+    benar: 2,
+    info: "🍓 Stroberi berwarna merah dan rasanya asam-manis."
+  },
+  {
+    gambar: "images/buah/semangka.jpg",
+    tanya: "🍉 Buah apakah ini?",
+    opsi: ["Melon", "Pepaya", "Semangka"],
+    benar: 2,
+    info: "🍉 Semangka banyak mengandung air."
+  },
+  {
+    gambar: "images/buah/melon.jpg",
+    tanya: "🍈 Buah apakah ini?",
+    opsi: ["Melon", "Semangka", "Pir"],
+    benar: 0,
+    info: "🍈 Melon rasanya manis dan segar."
+  },
+  {
+    gambar: "images/buah/nanas.jpg",
+    tanya: "🍍 Buah apakah ini?",
+    opsi: ["Durian", "Nanas", "Mangga"],
+    benar: 1,
+    info: "🍍 Nanas memiliki rasa manis dan asam."
+  },
+  {
+    gambar: "images/buah/pir.jpg",
+    tanya: "🍐 Buah apakah ini?",
+    opsi: ["Apel", "Pir", "Mangga"],
+    benar: 1,
+    info: "🍐 Pir berbentuk seperti lonceng."
+  },
+  {
+    gambar: "images/buah/pepaya.jpg",
+    tanya: "🍈 Buah apakah ini?",
+    opsi: ["Pepaya", "Melon", "Mangga"],
+    benar: 0,
+    info: "🍈 Pepaya baik untuk pencernaan."
+  },
+  {
+    gambar: "images/buah/durian.jpg",
+    tanya: "🥥 Buah apakah ini?",
+    opsi: ["Nangka", "Durian", "Kelapa"],
+    benar: 1,
+    info: "🥥 Durian dikenal sebagai raja buah."
+  },
+  {
+    gambar: "images/buah/nangka.jpg",
+    tanya: "🍈 Buah apakah ini?",
+    opsi: ["Cempedak", "Nangka", "Durian"],
+    benar: 1,
+    info: "🍈 Nangka berukuran besar dan beraroma kuat."
+  },
+  {
+    gambar: "images/buah/kelapa.jpg",
+    tanya: "🥥 Buah apakah ini?",
+    opsi: ["Kelapa", "Durian", "Alpukat"],
+    benar: 0,
+    info: "🥥 Kelapa memiliki air yang segar."
+  },
+  {
+    gambar: "images/buah/alpukat.jpg",
+    tanya: "🥑 Buah apakah ini?",
+    opsi: ["Alpukat", "Pir", "Mangga"],
+    benar: 0,
+    info: "🥑 Alpukat kaya lemak sehat."
+  }
+];
